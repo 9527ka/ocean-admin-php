433128a30183ed4411e28ec13ae90aaa6e5d234f
yj9527 <yj9527@qq.com>
2024-08-17 22:13:52 +0800

    gmail

--- config/mail.php
+++ config/mail.php
@@ -2,8 +2,8 @@
 
 return [
     'host' => 'ssl://smtp.gmail.com:465', // SMTP 服务器
-    'username' => 'kkgamesvip@gmail.com', // 你的邮箱地址
-    'password' => 'lzeuinpyllkzuplg', // 你的邮箱密码
+    'username' => 'foundationfou@gmail.com', // 你的邮箱地址
+    'password' => 'vtwerpvmjlbqngxc', // 你的邮箱密码
     'port' => 25, // SMTP 端口（Gmail 使用 587 或 465）
     'encryption' => 'tls', // 加密方式（Gmail 使用 tls）
     'from' => [
