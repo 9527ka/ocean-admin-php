bf943c29f25841133214418535bbc26c26ef54e9
xiaowu3156 <842177252@qq.com>
2024-08-19 23:03:39 +0800

    统计管理后台首页数据

--- config/console.php
+++ config/console.php
@@ -9,5 +9,16 @@ return [
         'crontab' => 'app\common\command\Crontab',
         // 退款查询
         'query_refund' => 'app\common\command\QueryRefund',
+
+        // 每日统计首页数据
+        'daily:statistics' => 'app\command\DailyStatistics',
+    ],
+
+    // 定时任务配置
+    'schedule' => [
+        [
+            'command' => 'daily:statistics',
+            'rule'    => '0 1 * * *',  // 每天凌晨1点运行
+        ],
     ],
 ];
